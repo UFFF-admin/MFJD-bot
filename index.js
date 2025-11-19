73ba36606048c6851a87ec48bb9e45a12c357e72
UFFF-admin <delisoal417+delisoal@gmail.com>
2025-11-19 19:58:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ client.on("messageCreate",function(message){
           name:"統一友愛戦線に栄光あれ！",
           type:0
         });
-        guild.channels.cache.forEach(function(channel){
+        guild.channels.cache.forEach(async function(channel){
           await channel.send(getText());
         });
       },500);
