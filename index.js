2d6a6e9f7554a4197e1b207b06ecce16591ef8c5
UFFF-admin <delisoal417+delisoal@gmail.com>
2025-11-19 15:07:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import express from "express";
-
 // ====== Discord Bot ======
-const client = new Client({
-  intents: [
+const client=new Client({
+  intents:[
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
   ],
 });
-
-client.once("ready", () => {
+client.once("ready",function(){
   console.log(`✅ Logged in as ${client.user.tag}`);
 });
-
-client.on("messageCreate", (message) => {
-  console.log(`[DEBUG] 受信: ${message.content}`);
-  if (message.author.bot) return;
-
-  if (message.content === "!ping") {
+client.on("messageCreate",function(message){
+  if(message.author.bot){
+    return;
+  }
+  if(message.content=="!ping") {
     message.reply("Pong!");
   }
 });
-
 // ====== HTTP server for Render ======
-const app = express();
-app.get("/", (req, res) => res.send("alive"));
-app.listen(process.env.PORT || 3000, () => {
+const app=express();
+app.get("/",function(req,res){
+  res.send("alive")
+});
+app.listen(process.env.PORT||3000,function(){
   console.log("🌐 Web server is running.");
 });
-
 // ====== Login ======
 client.login(process.env.DISCORD_TOKEN);
