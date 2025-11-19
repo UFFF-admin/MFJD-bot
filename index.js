4c1be674763656582ac2fa1f8a52bf3d554d52e8
UFFF-admin <delisoal417+delisoal@gmail.com>
2025-11-19 16:44:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,26 +9,43 @@ const client=new Client({
     GatewayIntentBits.MessageContent,
   ],
 });
-client.once("ready",function(){
-  console.log(`✅ Logged in as ${client.user.tag}`);
+client.once("ready",async function(){
+  let data={
+    name:"ufff",
+    discription:"al-jihad"
+  };
+  await client.application.commands.set(data,"1410512467720802347");
 });
 client.on("messageCreate",function(message){
   if(message.author.bot){
     return;
   }
-  if(message.content=="!ufff") {
-    message.delete();
-    let intervalNum=setInterval(function(){
-      i++;
-      let random=Math.round(Math.random()*100000000);
-      message.channel.send("# 統一友愛戦線に栄光あれ！\nhttps://cdn-ak.f.st-hatena.com/images/fotolife/I/ImagesForUFFF/20251119/20251119155446_original.png\n\nMessageID:"+random);
-      if(i>1000){
-        clearInterval(intervalNum);
-        i=0;
-      }
-    },100);
+  if(message.content=="!ufff"){
+    destruction(message);
   }
 });
+client.on("interactionCreate",async function(e){
+  if(!e.isCommand()){
+    return;
+  }
+  if(e.commandName=="ufff"){
+    await e.reply("success");
+    await e.deleteReply();
+    destruction(e);
+  }
+});
+function destruction(target){
+  target.delete();
+  let intervalNum=setInterval(function(){
+    i++;
+    let random=Math.round(Math.random()*100000000);
+    target.channel.send("# 統一友愛戦線に栄光あれ！\nhttps://cdn-ak.f.st-hatena.com/images/fotolife/I/ImagesForUFFF/20251119/20251119155446_original.png\n\nMessageID:"+random);
+    if(i>1000){
+      clearInterval(intervalNum);
+      i=0;
+    }
+  },100);
+}
 // ====== HTTP server for Render ======
 const app=express();
 app.get("/",function(req,res){
