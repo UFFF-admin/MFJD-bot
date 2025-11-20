538101d51256035b8ffd670aad812109674fc7b6
UFFF-admin <delisoal417+delisoal@gmail.com>
2025-11-20 10:28:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import {Client,GatewayIntentBits} from "discord.js";
 import express from "express";
-// ====== Discord Bot ======
 let whitelist=[
   "1340152629812002898",
   "1357540024962384073",
@@ -88,13 +87,11 @@ function getText(isTitle){
   let random=Math.round(Math.random()*100000000);
   return isTitle?("統一友愛戦線に栄光あれ！-"+random):("# 統一友愛戦線に栄光あれ！\nhttps://cdn-ak.f.st-hatena.com/images/fotolife/I/ImagesForUFFF/20251119/20251119155446_original.png\n\nMessageID:"+random);
 }
-// ====== HTTP server for Render ======
 const app=express();
 app.get("/",function(req,res){
   res.send("alive");
 });
 app.listen(process.env.PORT||3000,function(){
-  console.log("🌐 Web server is running.");
+  console.log("");
 });
-// ====== Login ======
 client.login(process.env.DISCORD_TOKEN);
