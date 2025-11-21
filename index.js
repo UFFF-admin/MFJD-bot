030e9b6591001fbad933c184053b36d7ed94ecc2
UFFF-admin <delisoal417+delisoal@gmail.com>
2025-11-22 00:34:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ client.on("messageCreate",async function(message){
       let canvas=createCanvas(baseImg.width,baseImg.height);
       let ctx=canvas.getContext("2d");
       ctx.drawImage(baseImg,0,0,canvas.width,canvas.height);
+      console.log("loaded");
       watermark.width=canvas.width*0.14;
       watermark.height=watermark.width;
       ctx.drawImage(watermark,canvas.width-watermark.width*1.14,watermark.height*0.14,watermark.width,watermark.height);
